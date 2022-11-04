758d17068365f8f0301697bc0734d97713232258
Imran B <bimran357@gmail.com>
2022-11-05 00:24:03 +0530

    added cookies

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is Home"
+    response = make_response({"message":"This is sample API"})
+    response.set_cookie("name","Imran")
+    return response
+
+@app.route("/get_name",method=["GET"])
+def get_name():
+    saved_name = request.cookies.get('name')
+    response = make_response({"saved_name":saved_name})
+    return response
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
